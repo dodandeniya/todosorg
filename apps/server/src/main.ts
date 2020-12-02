36ac8d301eb42567bcd77e0790178e6fb1f8dabb
Chamara Dodandeniya <92chamara@gmail.com>
2020-12-02 09:04:23 +0530

    build config

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -25,7 +25,7 @@ app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.SERVER_PORT || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
